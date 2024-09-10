b4da108d061946aa4c73e83a0dfc8fd8d4cd680b
Joar Warholm <joawar0713@skola.goteborg.se>
2024-09-10 08:44:43 +0200

    changed variabel name

--- ReadColu.py
+++ ReadColu.py
@@ -1,9 +1,9 @@
 f = open("pussel.csv", "r")
 lines = f.readlines()
 try:
-    what_column = int(input("What column would you like to see? ")) -1
+    col = int(input("What column would you like to see? ")) -1
     for line in lines:
-        first_element = line.strip().split(",")[what_column] 
+        first_element = line.strip().split(",")[col] 
         print(first_element)  
 except ValueError:
     print("No letters")
@@ -13,6 +13,3 @@ except:
     print("Something went wrong")
 
 f.close()
-
-
-#ValueError
